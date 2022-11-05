321e77da763ff4dab92e09511198654ef412430d
Ammar Vora <ammarmust4@gmail.com>
2022-11-05 17:05:58 -0400

    toggle works

--- bikemap/src/components/ToggleSwitch/ToggleSwitch.js
+++ bikemap/src/components/ToggleSwitch/ToggleSwitch.js
@@ -2,19 +2,42 @@ import React, { Component } from 'react';
 import './ToggleSwitch.scss';
 
 class ToggleSwitch extends Component {
+  state = {Mode: false};
+
+  handleMode = (mode) => {
+    if (mode) {
+      console.log('Driving');
+    } else {
+      console.log('Biking');
+    }
+
+    // Populate map with 
+
+
+
+  };
+
   render() {
     return (
       <div className="toggle-switch">
-        <input
-          type="checkbox"
-          className="toggle-switch-checkbox"
-          name={this.props.Name}
-          id={this.props.Name}
-        />
-        <label className="toggle-switch-label" htmlFor={this.props.Name}>
-          <span className="toggle-switch-inner" />
-          <span className="toggle-switch-switch" />
-        </label>
+            <input
+              type="checkbox"
+              className="toggle-switch-checkbox"
+              name={this.props.Name}
+              id={this.props.Name}
+              onClick={
+                
+                () => {
+                  this.setState({Mode: !this.state.Mode});
+                  this.handleMode(this.state.Mode);
+              }
+              
+              }
+            />
+            <label className="toggle-switch-label" htmlFor={this.props.Name}>
+              <span className="toggle-switch-inner" />
+              <span className="toggle-switch-switch" />
+            </label>
       </div>
     );
   }
